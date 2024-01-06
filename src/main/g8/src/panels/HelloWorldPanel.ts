e8a1bb97adffad01c8a310a6aeebb1184de54145
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-01-05 20:19:14 -0500

    update

--- src/main/g8/src/panels/HelloWorldPanel.ts
+++ src/main/g8/src/panels/HelloWorldPanel.ts
@@ -113,13 +113,13 @@ export class HelloWorldPanel {
         <head>
           <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-          <link rel="stylesheet" type="text/css" href="${stylesUri}">
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src \${webview.cspSource}; script-src 'nonce-\${nonce}';">
+          <link rel="stylesheet" type="text/css" href="\${stylesUri}">
           <title>Hello World</title>
         </head>
         <body>
           <div id="root"></div>
-          <script type="module" nonce="${nonce}" src="${scriptUri}"></script>
+          <script type="module" nonce="\${nonce}" src="\${scriptUri}"></script>
         </body>
       </html>
     `;
