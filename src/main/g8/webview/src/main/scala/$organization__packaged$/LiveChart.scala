629912f51f15323adfe3b1cf9cc62af5a86cd0a5
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-01-06 20:20:47 -0500

    Update LiveChart.scala
    took out an unneeded comment

--- src/main/g8/webview/src/main/scala/$organization__packaged$/LiveChart.scala
+++ src/main/g8/webview/src/main/scala/$organization__packaged$/LiveChart.scala
@@ -45,7 +45,7 @@ object Main:
     div(
       h1("Chart", img(src:= "/vite.svg")),
       div(child.text <-- g.summaryText,
-          child.text <-- g.focusedGridData.map(_.getOrElse("None").toString()) //TODO FIX THIS
+          child.text <-- g.focusedGridData.map(_.getOrElse("None").toString()) 
       ),
       g.htmlElement,
       div(child.text <-- g1.focusedGridData.map(_.getOrElse("None").toString())),
